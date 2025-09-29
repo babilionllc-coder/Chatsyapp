e2dfc7048ab71ba4dbef33295db196159cc2ea8c
babilionllc-coder <babilionllc@gmail.com>
2025-09-29 12:22:17 -0500

    Fix CocoaPods integration - include Pods directory and xcfilelist files for Xcode Cloud

---
+++ ios/Pods/Target Support Files/appvestor_billing_stats/appvestor_billing_stats-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_appvestor_billing_stats : NSObject
+@end
+@implementation PodsDummy_appvestor_billing_stats
+@end
---
+++ ios/Pods/Target Support Files/read_pdf_text/read_pdf_text-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_read_pdf_text : NSObject
+@end
+@implementation PodsDummy_read_pdf_text
+@end
